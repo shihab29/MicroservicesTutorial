209ea0b3b7b89d8959dbed86ed904876ece54339
Shihab Shahriar Ahmed <shihabshahriar59@gmail.com>
2022-11-26 00:28:09 +0600

    Initialize Product models & Product repository.

---
+++ MangoRestaurant/Mango.Services.ProductAPI/Repository/ProductRepository.cs
@@ -0,0 +1,32 @@
+﻿using Mango.Services.ProductAPI.Models.Dto;
+
+namespace Mango.Services.ProductAPI.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        public ProductRepository()
+        {
+
+        }
+
+        public Task<ProductDto> CreateUpdateProduct(ProductDto productDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteProduct(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProductDto> GetProductById(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ProductDto>> GetProducts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
